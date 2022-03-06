b2eeeb60b479b23c19a0be379cd44a210a87f4d7
deven7818 <deven7818@gmail.com>
2022-03-06 13:18:32 +0530

    Insert element to Binary search tree

---
+++ BinarySearchTree/src/main/java/com/binarysearchtree/BNode.java
@@ -0,0 +1,18 @@
+package com.binarysearchtree;
+
+public class BNode {
+	int data;
+	BNode left, right;
+	
+	/**
+	 * Parameterized Constructor
+	 */
+	public BNode(int data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+
+	
+}
+
